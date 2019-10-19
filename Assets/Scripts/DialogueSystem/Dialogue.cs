5870c07ba12940c328a86a352a7e3aa292932f3c
Simon <666ohyeah999@gmail.com>
2019-10-19 16:59:29 +0200

    :construction: Update dialogue system

---
+++ Assets/Scripts/DialogueSystem/Dialogue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    [SerializeField] [TextArea(1, 3)] private string[] sentences;
+    [SerializeField] private GameObject bubble;
+    [SerializeField] private float heightBubble;
+    private bool triggered = false;
+
+    public void TriggerDialogue()
+    {
+        if (!triggered)
+        {
+            DialogueManager thisBubble = GameObject.Instantiate(bubble, transform.position + heightBubble * Vector3.up, Quaternion.identity, transform).GetComponent<DialogueManager>();
+            thisBubble.GiveDialogues(sentences);
+            triggered = true;
+        }
+    }
+}
--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -1,46 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public enum DialogueStyle {basic, fast};
 
 public class DialogueManager : MonoBehaviour
 {
     private DialogueStyle style = DialogueStyle.basic;
-    private Queue<string> sentences;
+
+    private Queue<string> sentences = new Queue<string>();
+
     private string currentSentence;
     private int currentPosition;
     private float counter;
+    private TextMeshProUGUI text;
+    private bool active = false;
+    private bool autoPass = true;
 
-    [SerializeField] private float slowLetterDuration;
-    [SerializeField] private float fastLetterDuration;
+    [SerializeField] private float slowLetterDuration = 0.05f;
+    [SerializeField] private float fastLetterDuration = 0.2f;
+    private float textDuration = 2f;
 
 
 
     private void Start()
     {
         counter = 0.0f;
+        text = GetComponentInChildren<TextMeshProUGUI>();
     }
 
     public void PassDialogue()
     {
-        if (sentences.Count > 0 && currentPosition == currentSentence.Length - 1)
+        if (sentences.Count > 0 && currentPosition == sentences.Peek().Length)
         {
-            currentSentence = "";
-            sentences.Dequeue();
-            currentPosition = 0;
-            counter = 0.0f;
+            if (sentences.Count > 0)
+            {
+                currentSentence = "";
+                sentences.Dequeue();
+                currentPosition = 0;
+                counter = 0.0f;
+            }
         }
+        else if (sentences.Count == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void GiveDialogues(string[] theseSentences, bool auto = true)
+    {
+        if (theseSentences == null)
+        {
+            return;
+        }
+        for(int i = 0; i < theseSentences.Length; ++i)
+        {
+            sentences.Enqueue(theseSentences[i]);
+        }
+        active = true;
+        currentPosition = 0;
+        currentSentence = "";
+        counter = 0.0f;
+        autoPass = auto;
     }
     
     private void Update()
     {
-        counter += Time.deltaTime;
-        if ((style != DialogueStyle.fast && counter > slowLetterDuration) || (style == DialogueStyle.fast && counter > fastLetterDuration))
+        if (active && sentences.Count > 0)
         {
-            counter = 0.0f;
-            ++currentPosition;
-            currentSentence = sentences.Peek().Substring(0,currentPosition);
+            counter += Time.deltaTime;
+            if ((currentPosition < sentences.Peek().Length) && ((style != DialogueStyle.fast && counter > slowLetterDuration) || (style == DialogueStyle.fast && counter > fastLetterDuration)))
+            {
+                counter = 0.0f;
+                if (sentences.Peek().Substring(currentPosition, 1).Equals("*"))
+                {
+                    ++currentPosition;
+                    style = style == DialogueStyle.fast ? DialogueStyle.basic : DialogueStyle.fast;
+                }
+                else
+                {
+                    currentSentence = currentSentence + sentences.Peek().Substring(currentPosition, 1);
+                    ++currentPosition;
+                    text.text = currentSentence;
+                }
+            }
+            else if(counter > textDuration && autoPass)
+            {
+                PassDialogue();
+            }
+        } else if(sentences.Count == 0 && active)
+        {
+            PassDialogue();
         }
     }
 }
