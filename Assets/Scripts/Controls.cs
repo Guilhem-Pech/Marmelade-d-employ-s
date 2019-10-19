2b3d66bb953ea84948d95422e5734684958b048f
Guilhem PECH <guilhem.pech@gmail.com>
2019-10-19 12:30:44 +0200

    :construction: Create input binding and simple movement

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,245 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class Controls : IInputActionCollection, IDisposable
+{
+    private InputActionAsset asset;
+    public Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""92f46e6a-3486-4e89-b5d3-dcf727b5b0f4"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""aef82dbb-b320-4162-b705-c8c679e15faa"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""MousePos"",
+                    ""type"": ""Value"",
+                    ""id"": ""d9a83813-0281-41f9-8931-ec24ff855368"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Action"",
+                    ""type"": ""Button"",
+                    ""id"": ""35898d19-173d-429f-b2ed-47939a684dea"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": ""Press""
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""AD"",
+                    ""id"": ""aeff752d-3d50-48b4-b281-4a9d3e95aaf1"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""073233fa-7910-4415-acc3-871a5571e4f5"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""bf28704b-7ed3-409c-aa59-012614b359e2"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Arrows"",
+                    ""id"": ""2a014a5e-3a44-42f6-b53c-f5197fc7e735"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""2f62440b-d86d-44dc-bc78-056ab46a09ff"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""929ce328-f5f5-42a6-adfd-99fc5fe40eee"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c21eafb5-5fe3-4669-9d54-0c1927bd2463"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MousePos"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""77e4a882-1370-4cc0-8b6a-fd0241adc4c4"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Action"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_MousePos = m_Player.FindAction("MousePos", throwIfNotFound: true);
+        m_Player_Action = m_Player.FindAction("Action", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_MousePos;
+    private readonly InputAction m_Player_Action;
+    public struct PlayerActions
+    {
+        private Controls m_Wrapper;
+        public PlayerActions(Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @MousePos => m_Wrapper.m_Player_MousePos;
+        public InputAction @Action => m_Wrapper.m_Player_Action;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                MousePos.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMousePos;
+                MousePos.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMousePos;
+                MousePos.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMousePos;
+                Action.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAction;
+                Action.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAction;
+                Action.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAction;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                Move.started += instance.OnMove;
+                Move.performed += instance.OnMove;
+                Move.canceled += instance.OnMove;
+                MousePos.started += instance.OnMousePos;
+                MousePos.performed += instance.OnMousePos;
+                MousePos.canceled += instance.OnMousePos;
+                Action.started += instance.OnAction;
+                Action.performed += instance.OnAction;
+                Action.canceled += instance.OnAction;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnMousePos(InputAction.CallbackContext context);
+        void OnAction(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speedModifier = 1;
+    private float XAxis = 0f;
+    private Vector2 mousePos = Vector2.zero;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    public void OnMouseChangePos(InputAction.CallbackContext context)
+    {
+        mousePos = context.ReadValue<Vector2>();
+        // TODO Do a feedback when we hover an NPC / Something we can interact
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            // TODO Open dialogue or something
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        XAxis = context.ReadValue<float>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.velocity = speedModifier * XAxis * Vector2.right;
+    }
+}
