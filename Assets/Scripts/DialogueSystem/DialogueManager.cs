d092f55f3e07b6beb0560bacacd7ce16589a5b5a
Simon <666ohyeah999@gmail.com>
2019-10-20 17:45:38 +0200

    :sparkles: New sounds + barks

--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public enum DialogueStyle {basic, fast};
+public enum DialogueStyle {basic, slow, ultraslow};
 
 public class DialogueManager : MonoBehaviour
 {
@@ -28,8 +28,9 @@ public class DialogueManager : MonoBehaviour
 
 
     [SerializeField] private int[] voice;
-    [SerializeField] private float slowLetterDuration = 0.06f;
-    [SerializeField] private float fastLetterDuration = 0.006f;
+    [SerializeField] private float ultraSlowLetterDuration = 0.25f;
+    [SerializeField] private float slowLetterDuration = 0.1f;
+    [SerializeField] private float fastLetterDuration = 0.04f;
     [SerializeField] private GameObject buttonPrefab;
 
 
@@ -128,13 +129,18 @@ public class DialogueManager : MonoBehaviour
         if (active && sentences.Count > 0)
         {
             counter += Time.deltaTime;
-            if ((currentPosition < sentences.Peek().Length) && ((style != DialogueStyle.fast && counter > slowLetterDuration) || (style == DialogueStyle.fast && counter > fastLetterDuration)))
+            if ((currentPosition < sentences.Peek().Length) && ((style == DialogueStyle.slow && counter > slowLetterDuration) || (style == DialogueStyle.ultraslow && counter > ultraSlowLetterDuration) || (style == DialogueStyle.basic && counter > fastLetterDuration)))
             {
                 counter = 0.0f;
                 if (sentences.Peek().Substring(currentPosition, 1).Equals("*"))
                 {
                     ++currentPosition;
-                    style = style == DialogueStyle.fast ? DialogueStyle.basic : DialogueStyle.fast;
+                    style = style == DialogueStyle.slow ? DialogueStyle.basic : DialogueStyle.slow;
+                }
+                else if (sentences.Peek().Substring(currentPosition, 1).Equals("#"))
+                {
+                    ++currentPosition;
+                    style = style == DialogueStyle.ultraslow ? DialogueStyle.basic : DialogueStyle.ultraslow;
                 }
                 else
                 {
