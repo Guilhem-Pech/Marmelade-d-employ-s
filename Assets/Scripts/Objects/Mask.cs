22a1072493f67375f278b00b471dc0e81d3a5037
Guilhem PECH <guilhem.pech@gmail.com>
2019-10-20 16:02:30 +0200

    :art: :sparkles: Add pnj to the scene

---
+++ Assets/Scripts/Objects/Mask.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mask : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private SpriteRenderer _spriteRenderer;
+    
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.enabled = true;
+    }
+
+    // Update is called once per fram
+}
