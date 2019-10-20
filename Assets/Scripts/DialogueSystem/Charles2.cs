07b77c50a86d0e7292a5aa57d4e0e1de53cad4a7
Simon <666ohyeah999@gmail.com>
2019-10-20 21:50:50 +0200

    :sparkles: Stacy  + Charles dialogues

---
+++ Assets/Scripts/DialogueSystem/Charles2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charles2 : DialogueWithChoice
+{
+    public override void TriggerAtEndOfDialogue()
+    {
+        //Alicia se lève
+    }
+}
