58d3249acab4d0a71b9aae45fff16a22de3ba73f
Simon <666ohyeah999@gmail.com>
2019-10-21 16:02:48 +0200

    :sparkles: Bip bip ; c'est le portique

---
+++ Assets/Scripts/DialogueSystem/Stacy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stacy : Dialogue
+{
+    public Gate gate;
+    public override void TriggerDialogue()
+    {
+        if (!triggered)
+        {
+            DialogueManager thisBubble = GameObject.Instantiate(bubble, transform.position + heightBubble * Vector3.up, Quaternion.identity, transform).GetComponent<DialogueManager>();
+            thisBubble.GiveDialogues(sentences, this, voice);
+            triggered = true;
+            gate.Open();
+        }
+    }
+}
